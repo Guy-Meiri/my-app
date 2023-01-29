a74388ff075cb2c0b093fabc4c8dcca6535b1a26
Guy Meiri <guymeiri@microsoft.com>
2023-01-30 01:23:46 +0200

    commit message

--- src/RefExampleBefore.tsx
+++ src/RefExampleBefore.tsx
@@ -1,22 +1,13 @@
-import { useRef } from 'react';
 import Form from './Form';
 
 export default function RefExampleBefore() {
-
-    const inputRef = useRef<HTMLInputElement>(null);
-
-    const focusOnInput = () => {
-        if (inputRef.current) {
-            inputRef.current?.focus();
-        }
-    }
     return (
         <div className='flex'>
             <div className=' form flex'>
                 <Form />
-                <button onClick={focusOnInput}> Submit</button>
+                <button> Submit</button>
             </div>
-            <input ref={inputRef} placeholder='next' />
+            <input placeholder='focus on me!' />
         </div>
     )
 }
--- src/Root.tsx
+++ src/Root.tsx
@@ -7,8 +7,8 @@ export function Root() {
 
   return (
     <>
-      {/* <RefExampleBefore /> */}
-      <ScrollExample />
+      <RefExampleBefore />
+      {/* <ScrollExample /> */}
     </>
   );
 }
